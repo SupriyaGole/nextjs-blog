f93a153364434e6972addb73dca4bba788731d32
Supriya Gole <supriya.anandrao.gole@zalando.de>
2023-03-09 23:49:41 +0100

    feat: Add a sample blog website using nextjs

---
+++ pages/posts/first-post.js
@@ -0,0 +1,16 @@
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
+import Head from 'next/head';
+import Layout from "../../components/layout";
+
+export default function FirstPost () {
+    return <Layout>
+        <Head>
+            <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2 className={styles.title}>
+            <Link href="/">Back to Home</Link>
+        </h2>
+    </Layout>
+}
